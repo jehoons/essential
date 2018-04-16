5117c1d4562c5cc7c7344034a14f5c7a6e07057c
Je-Hoon Song <song.jehoon@gmail.com>
2018-04-16 22:53:29 +0900

    .

---
+++ share/test/test_tensorflow_hello.py
@@ -0,0 +1,37 @@
+import sys
+import numpy as np
+import tensorflow as tf
+from datetime import datetime
+
+#device_name = sys.argv[1]  # Choose device from cmd line. Options: gpu or cpu
+#shape = (int(sys.argv[2]), int(sys.argv[2]))
+shape = (100,100) 
+device_name = '/device:GPU:0'
+
+
+from tensorflow.python.client import device_lib
+
+device_lib.list_local_devices()
+
+# device_name = '/cpu:0'
+#if device_name == "gpu":
+#    device_name = "/gpu:0"
+#else:
+#    device_name = "/cpu:0"
+with tf.device(device_name):
+    random_matrix = tf.random_uniform(shape=shape, minval=0, maxval=1)
+    dot_operation = tf.matmul(random_matrix, tf.transpose(random_matrix))
+    sum_operation = tf.reduce_sum(dot_operation)
+
+startTime = datetime.now()
+with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
+        result = session.run(sum_operation)
+        print(result)
+
+# It can be hard to see the results on the terminal with lots of output -- add some newlines to improve readability.
+print("\n" * 5)
+print("Shape:", shape, "Device:", device_name)
+print("Time taken:", datetime.now() - startTime)
+
+print("\n" * 5)
+
